4191151fd5c0ac7f68ea5d1fab86c71d07e8f484
Attila Bencze <attila.bencze@amersports.com>
2020-01-15 15:00:39 +0200

    Fix: iTerm2 color issue

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,13 @@ set DIR (dirname (status --current-filename))
 
 source $DIR"/.aliases"
 source $DIR"/.variables"
+
+# clear previously set color variables
+for color_variable in (set -n | grep -e pure_color_)
+    set -e $color_variable
+end
+
+# override pure theme gray color to be compatible with solarized dark theme of iTerm2
+set -g pure_color_mute (set_color yellow)
+
+source "$DIR"/conf.d/pure.fish
